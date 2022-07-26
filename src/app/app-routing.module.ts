e615fe311b62efeca669bc657ed315c2a28de303
amenifitouri <fitouriamenii@gmail.com>
2022-07-26 15:41:56 +0200

    login page path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { AuthGuard } from './shared/auth/auth-guard.service';
 const appRoutes: Routes = [
   {
     path: '',
-    redirectTo: 'tables/smart',
+    redirectTo: 'pages/login',
     pathMatch: 'full',
   },
   { path: '', component: FullLayoutComponent, data: { title: 'full Views' }, children: Full_ROUTES, canActivate: [AuthGuard] },

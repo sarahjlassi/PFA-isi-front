639c4e45e47526abd5ad3729af00637f4b2cb1a2
amenifitouri <fitouriamenii@gmail.com>
2022-08-02 22:39:53 +0200

    reserv filtage 1

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/service/auth.service';
 import { Router } from '@angular/router';
 import { TokenStorageService } from 'app/service/token-storage.service';
+import{ReservationService} from '../service/reservation.service';
+import { Reservation } from 'app/models/reservation.model';
+import { NgbPaginationNumber } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  resers:any;
+  Ndispo:any[]=[];
+ 
   date :Date=new Date();
   settings={
     bigBanner:true,
@@ -15,24 +21,46 @@ export class UserComponent implements OnInit {
     format:'dd-MM-yyyy hh:mm a',
     defaultOpen:'false',
     closeOnSelect:false
-  }
-  showAdminBoard = false;
-  showUserBoard = false;
+  };
+  dateR:any;
   username:any;
   email:any;
-  isLoggedIn = false;
-  isLoginFailed = false;
-  errorMessage = '';
+  taille:any;
   roles: string[] = [];
 
-  constructor(private authService: AuthService, private _router: Router, private tokenStorage: TokenStorageService) { }
+  constructor(private authService: AuthService, private ReservationService:ReservationService,private _router: Router, private tokenStorage: TokenStorageService) { }
   currentPage: string ="TimeLine"
   ngOnInit() {
+    this.ReservationService.getListReservations().subscribe(
+        
+      data=>{
+        
+        
+        this.resers=data
+        this.taille=this.resers.length;
+
+      }),
+
     this.username=this.tokenStorage.getUser().username;
     this.email=this.tokenStorage.getUser().email;
     console.log( this.username);
   }
   showPage(page: string) {
     this.currentPage = page;
+}
+
+
+submit(){
+  console.log(this.dateR)
+  console.log(this.resers)
+  console.log( this.taille)
+  for(let i=0;i<=this.taille;i++){
+   
+    if(this.resers[i].date==this.dateR){
+
+      this.Ndispo.push(this.resers[i].salle.id)
+    }
+  }
+  console.log("salle non dispo", this.Ndispo);
 }
 }

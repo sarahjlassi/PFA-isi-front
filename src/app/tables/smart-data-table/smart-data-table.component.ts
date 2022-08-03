5456179bd81465733b4a202c518c063eba108c83
Sarah Jlassi <sarrajlasi93@gmail.com>
2022-08-04 00:02:26 +0100

    roles models

--- src/app/tables/smart-data-table/smart-data-table.component.ts
+++ src/app/tables/smart-data-table/smart-data-table.component.ts
@@ -31,8 +31,10 @@ export class SmartTableComponent {
         private modalService: NgbModal
       )  { this.pay=new User();}
     ngOnInit() {
+      
         this.userService.getListUsers().subscribe(data=>
-          this.pays=data)
+          this.pays=data);
+          console.log("hhhhhhhh",data);
       }
       onSubmit(){
         this.userService.adduser(this.pay).subscribe
@@ -71,6 +73,9 @@ export class SmartTableComponent {
         } else {
           return `with: ${reason}`;
         }
+
+
+        
       }
 
 
